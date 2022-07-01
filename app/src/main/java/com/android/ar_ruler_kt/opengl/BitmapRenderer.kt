9d0a335adb72d0c041894df6ec37e1691d15642a
贾天龙 <604872657@qq.com>
2022-07-01 14:36:23 +0800

    bitmap 可以渲染

--- app/src/main/java/com/android/ar_ruler_kt/opengl/BitmapRenderer.kt
+++ app/src/main/java/com/android/ar_ruler_kt/opengl/BitmapRenderer.kt
@@ -16,31 +16,42 @@ import java.nio.FloatBuffer
  * @detail：
  */
 class BitmapRenderer(context: Context) : BaseRenderer(context) ,IBaseRenderer{
-    override var fragmentPath: String = "shader/background_show_camera.frag"
-    override var vertexPath: String = "shader/background_show_camera.vert"
+    override var fragmentPath: String = "shader/bitmap_shader.frag"
+    override var vertexPath: String = "shader/bitmap_shader.vert"
+//    val bitmap:Bitmap by lazy { BitmapFactory.decodeStream(context.assets.open("test.webp")) }
     val bitmap:Bitmap by lazy { BitmapFactory.decodeStream(context.assets.open("pointCircle.png")) }
     init {
         textureTarget =GLES30.GL_TEXTURE_2D
     }
 
-    var u_CameraColorTexture = -1
+    var u_ColorTexture = -1
     var a_Position = -1
-    var a_CameraTexCoord = -1
+    var a_ColorTexCoord = -1
 
-    //默认顶点坐标
+    /**
+     * 顶点坐标
+     */
+//    private val vertexCoords = floatArrayOf(
+//        -1.0f, -1.0f,
+//        -1.0f, +1.0f,
+//        +1.0f, -1.0f,
+//        +1.0f, +1.0f,
+//    )
+    private val threshold = 0.3f
     private val vertexCoords = floatArrayOf(
-        -1.0f, -1.0f, -1.0f, +1.0f, +1.0f, -1.0f, +1.0f, +1.0f
+        -threshold, -threshold,
+        -threshold, +threshold,
+        +threshold, -threshold,
+        +threshold, +threshold,
     )
-
     /**
      * 纹理坐标
      */
     private val textureCoord = floatArrayOf(
-        0.0f, 0.0f,
         0.0f, 1.0f,
+        0.0f, 0.0f,
+        1.0f, 1.0f,
         1.0f, 0.0f,
-
-        1.0f, 1.0f
     )
     val vertexBuffer: FloatBuffer by lazy {
         val buffer: ByteBuffer = ByteBuffer.allocateDirect(this.vertexCoords.size * 4)
@@ -59,13 +70,12 @@ class BitmapRenderer(context: Context) : BaseRenderer(context) ,IBaseRenderer{
     }
 
     override fun initShaderParameter() {
-        u_CameraColorTexture =  GLES30.glGetUniformLocation(program,"u_CameraColorTexture")
+        u_ColorTexture =  GLES30.glGetUniformLocation(program,"u_ColorTexture")
         a_Position =  GLES30.glGetAttribLocation(program,"a_Position")
-        a_CameraTexCoord =  GLES30.glGetAttribLocation(program,"a_CameraTexCoord")
+        a_ColorTexCoord =  GLES30.glGetAttribLocation(program,"a_ColorTexCoord")
         GLError.maybeThrowGLException("initShaderParameter", "initShaderParameter$program")
 
-        Log.w(TAG,"$TAG,  a_Position:$a_Position    a_CameraTexCoord:$a_CameraTexCoord    u_CameraColorTexture:$u_CameraColorTexture")
-
+        Log.w(TAG,"$TAG,  a_Position:$a_Position    a_ColorTexCoord:$a_ColorTexCoord    u_ColorTexture:$u_ColorTexture")
     }
 
     override fun onSurfaceCreated() {
@@ -77,6 +87,7 @@ class BitmapRenderer(context: Context) : BaseRenderer(context) ,IBaseRenderer{
         GLUtils.texImage2D(textureTarget, 0, bitmap, 0)
         GLError.maybeThrowGLException("BackgroundRenderer", "onDrawFrame")
         GLES30.glBindTexture(textureTarget,0)
+        GLError.maybeThrowGLException(TAG, "onDrawFrame")
     }
 
     override fun onSurfaceChanged(width: Int, height: Int) {
@@ -86,23 +97,21 @@ class BitmapRenderer(context: Context) : BaseRenderer(context) ,IBaseRenderer{
 
     override fun onDrawFrame() {
         GLError.maybeThrowGLException(TAG, "onDrawFrame")
-        GLES30.glActiveTexture(GLES30.GL_TEXTURE0)
         GLES30.glUseProgram(program)
         GLES30.glBindTexture(textureTarget,textureIds[0])
-        GLES30.glUniform1i(u_CameraColorTexture, 0)
+        GLES30.glUniform1i(u_ColorTexture, 0)
         GLES30.glEnableVertexAttribArray(a_Position)
-        GLES30.glEnableVertexAttribArray(a_CameraTexCoord)
+        GLES30.glEnableVertexAttribArray(a_ColorTexCoord)
 
 
-        GLES30.glVertexAttribPointer(a_Position, 2, GLES30.GL_FLOAT, false, 0,
-            this.vertexBuffer
+        GLES30.glVertexAttribPointer(a_Position, 2, GLES30.GL_FLOAT, false, 0, this.vertexBuffer
         )
-        GLES30.glVertexAttribPointer(a_CameraTexCoord, 2, GLES30.GL_FLOAT, false, 0, textureBuffer)
-        GLUtils.texImage2D(textureTarget, 0, bitmap, 0)
+        GLES30.glVertexAttribPointer(a_ColorTexCoord, 2, GLES30.GL_FLOAT, false, 0, textureBuffer)
+//        GLUtils.texImage2D(textureTarget, 0, bitmap, 0)
         GLES30.glDrawArrays(GLES30.GL_TRIANGLE_STRIP, 0, 4)
 
         GLES30.glDisableVertexAttribArray(a_Position)
-        GLES30.glDisableVertexAttribArray(a_CameraTexCoord)
+        GLES30.glDisableVertexAttribArray(a_ColorTexCoord)
 
         GLES30.glBindTexture(textureTarget, 0)
         GLES30.glUseProgram(0)
